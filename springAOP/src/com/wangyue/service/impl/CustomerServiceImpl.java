1153d235a2a80eeb55836997a8ce31dbddc6c939
qq793199244 <793199244@qq.com>
2018-07-14 21:23:48 +0800

    基于xml的AOP配置步骤及spring中的切入点表达式

---
+++ springAOP/src/com/wangyue/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,31 @@
+package com.wangyue.service.impl;
+
+/**
+ * 模拟客户业务层的实现类
+ */
+
+import com.wangyue.service.ICustomerService;
+import com.wangyue.utils.Logger;
+
+public class CustomerServiceImpl implements ICustomerService {
+
+	@Override
+	public void saveCustomer() {
+		//new Logger().printLog();
+		System.out.println("保存了客户。。");
+
+	}
+
+	@Override
+	public void updateCustomer(int i) {
+		System.out.println("更新了客户。。" + i);
+
+	}
+
+	@Override
+	public int deleteCustomer() {
+		System.out.println("删除了客户。。");
+		return 0;
+	}
+
+}
---
+++ springAOP/src/com/wangyue/ui/Client.java
@@ -0,0 +1,20 @@
+package com.wangyue.ui;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.wangyue.service.ICustomerService;
+
+public class Client {
+
+	public static void main(String[] args) {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+		ICustomerService cs = (ICustomerService) ac.getBean("customerService");
+		cs.saveCustomer();
+		cs.updateCustomer(1);
+		cs.deleteCustomer();
+
+	}
+
+}
