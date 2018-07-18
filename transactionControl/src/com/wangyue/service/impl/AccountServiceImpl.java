949fceb019025b70baa04744eaa7e399a8c348ee
qq793199244 <793199244@qq.com>
2018-07-18 23:01:26 +0800

    Spring中的事务控制准备环境

---
+++ transactionControl/src/com/wangyue/service/impl/AccountServiceImpl.java
@@ -0,0 +1,34 @@
+package com.wangyue.service.impl;
+
+import com.wangyue.dao.IAccountDao;
+import com.wangyue.domain.Account;
+import com.wangyue.service.IAccountService;
+
+public class AccountServiceImpl implements IAccountService {
+	
+	private IAccountDao accountDao;
+
+	public void setAccountDao(IAccountDao accountDao) {
+		this.accountDao = accountDao;
+	}
+
+	@Override
+	public void transfer(String sourceName, String targetName, Float money) {
+		Account source = accountDao.findAccountByName(sourceName);
+		Account target = accountDao.findAccountByName(targetName);
+		
+		source.setMoney(source.getMoney()-money);
+		target.setMoney(target.getMoney()+money);
+		
+		accountDao.updateAccount(source);
+		accountDao.updateAccount(target);
+
+	}
+
+	@Override
+	public Account findAccountById(Integer accountId) {
+		
+		return accountDao.findAccountById(accountId);
+	}
+
+}
