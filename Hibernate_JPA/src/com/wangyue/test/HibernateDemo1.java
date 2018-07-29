b3e144d0b3fa59651eee03726952d769ec1b66ae
qq793199244 <793199244@qq.com>
2018-07-30 00:26:46 +0800

    Hibernate中使用JPA注解映射配置

---
+++ Hibernate_JPA/src/com/wangyue/test/HibernateDemo1.java
@@ -0,0 +1,74 @@
+package com.wangyue.test;
+
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.junit.Test;
+
+import com.wangyue.domain.Customer;
+import com.wangyue.utils.HibernateUtil;
+
+/**
+ * 使用JPA配置映射 使用Hibernate操作
+ * @author WY
+ *
+ */
+
+public class HibernateDemo1 {
+	
+	/**
+	 * 保存
+	 */
+	@Test
+	public void test1(){
+		Customer c = new Customer();
+		c.setCustName("hibernate jpa customer");
+		
+		Session s = HibernateUtil.getCurrentSession();
+		Transaction tx = s.beginTransaction();
+		s.save(c);
+		tx.commit();
+	}
+	
+	/**
+	 * 查询一个
+	 */
+	@Test
+	public void test2(){
+		
+		Session s = HibernateUtil.getCurrentSession();
+		Transaction tx = s.beginTransaction();
+		Customer c = s.get(Customer.class, 6L);
+		System.out.println(c);
+		tx.commit();
+	}
+	
+	/**
+	 * 修改
+	 */
+	@Test
+	public void test3(){
+		
+		Session s = HibernateUtil.getCurrentSession();
+		Transaction tx = s.beginTransaction();
+		Customer c = s.get(Customer.class, 6L);
+		c.setCustAddress("唐山");
+		tx.commit();
+	}
+	
+	
+	/**
+	 * 删除操作
+	 */
+	@Test
+	public void test4(){
+		
+		Session s = HibernateUtil.getCurrentSession();
+		Transaction tx = s.beginTransaction();
+		Customer c = s.get(Customer.class, 6L);
+		s.delete(c);
+		tx.commit();
+	}
+
+
+}
